9ae79d2a39175a4ce270837a7ccffe8f472adef6
oriolfornos <oriol-1-2@hotmail.com>
2020-09-30 18:20:02 +0200

    Afeguida la funcio foo

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+
+        function of_foo($arg_1, $arg_2)
+        {
+            echo "Función de ejemplo.\n";
+            return $valor_devuelto;
+        }
+        
+        /*
+        function of_suma($arg_1, $arg_2)
+        {
+            echo "Función de suma.\n";  
+        } */
+        
+        /* function of_resta($arg_1, $arg_2)
+        {
+            echo "Función de resta.\n";
+            return $valor_devuelto;
+        } */
+        
+        ?>
+    </body>
+</html>
